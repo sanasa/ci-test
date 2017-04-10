ac3d6a8c05518638e58eb31f847e3932a9ded08c
SafaMjn <safa.morjane@gmail.com>
2017-04-10 15:56:15 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
 
-import { ProjectListComponent } from './project-list/project-list.component';
-import { AddProjectComponent } from './add-project/add-project.component';
-
-import { EmitterService } from './services/emitter.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,13 +7,5 @@ import { EmitterService } from './services/emitter.service';
   
 })
 export class AppComponent {
-
-	public host_id: "HOST_COMPONENT";
-	public title:string = 'Angular 2, Nodejs & MongoDB CRUD';
-
-	private projectInfo = 'CRUD_Project_INFO';
-    private reset = 'CRUD_RESET_FORM';
-    private projectList = 'CRUD_Project_LIST';
-
-	constructor(private _emitterService: EmitterService) {}
+  title = 'This is an angular 2 app!';
 }
